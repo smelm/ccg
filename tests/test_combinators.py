21f25ab042d475b7cc95fe56101cd5f11172d620
Samuel Melm <samuel.melm@stud.uni-heidelberg.de>
2022-02-28 10:15:18 +0100

    test(combinators): type raising

--- tests/test_combinators.py
+++ tests/test_combinators.py
@@ -9,26 +9,68 @@ X, Y, Z = LexiconBuilder().primitive_categories("X", "Y", "Z")
 FORWARD = Direction.LEFT.value
 BACKWARD = Direction.RIGHT.value
 
-# double __ mean parenthesis
-# direction indicated through alphabetical order
+CAT = dict()
 
-CATEGORIES = dict()
+CAT["X/Y"] = unwrap_builder(X << Y)
+CAT["Y/Z"] = unwrap_builder(Y << Z)
+CAT["X/Z"] = unwrap_builder(X << Z)
+CAT["X/(Y/Z)"] = unwrap_builder(X << (Y << Z))
+CAT["X\\Y"] = unwrap_builder(Y >> X)
+CAT["X\\(Y/Z)"] = unwrap_builder((Y << Z) >> X)
+CAT["(Y\\X)/Z"] = unwrap_builder((X >> Y) << Z)
+CAT["(X/Y)\\(X/Y)"] = unwrap_builder((X << Y) >> (X << Y))
 
-CATEGORIES["X/Y"] = unwrap_builder(X << Y)
-CATEGORIES["Y/Z"] = unwrap_builder(Y << Z)
-CATEGORIES["X/Z"] = unwrap_builder(X << Z)
-CATEGORIES["X/(Y/Z)"] = unwrap_builder(X << (Y << Z))
-CATEGORIES["X\\Y"] = unwrap_builder(Y >> X)
-CATEGORIES["X\\(Y/Z)"] = unwrap_builder((Y << Z) >> X)
-# CATEGORIES["X/(Y\\Z)"] = unwrap_builder(X << (Z >> Y))
+CAT["X"] = unwrap_builder(X)
+CAT["Y"] = unwrap_builder(Y)
+CAT["Z"] = unwrap_builder(Z)
 
-X = unwrap_builder(X)
-Y = unwrap_builder(Y)
-Z = unwrap_builder(Z)
 
-CATEGORIES["X"] = X
-CATEGORIES["Y"] = Y
-CATEGORIES["Z"] = Z
+class TestCombinators:
+    def test_forward_application(self):
+        combines_only(
+            Combinators.FORWARD_APPLICATION.value,
+            [
+                (CAT["X/Y"], CAT["Y"], CAT["X"]),
+                (CAT["Y/Z"], CAT["Z"], CAT["Y"]),
+                (CAT["X/(Y/Z)"], CAT["Y/Z"], CAT["X"]),
+                (CAT["X/Z"], CAT["Z"], CAT["X"]),
+                (CAT["(Y\\X)/Z"], CAT["Z"], unwrap_builder(X >> Y)),
+            ],
+        )
+
+    def test_backward_application(self):
+        combines_only(
+            Combinators.BACKWARD_APPLICATION.value,
+            [
+                (CAT["Y"], CAT["X\\Y"], CAT["X"]),
+                (CAT["Y/Z"], CAT["X\\(Y/Z)"], CAT["X"]),
+                (CAT["X/Y"], CAT["(X/Y)\\(X/Y)"], CAT["X/Y"]),
+            ],
+        )
+
+    def test_forward_composition(self):
+        combines_only(
+            Combinators.FORWARD_COMPOSITION.value,
+            [
+                (CAT["X/Y"], CAT["Y/Z"], CAT["X/Z"]),
+            ],
+        )
+
+    def test_backward_composition(self):
+        combines_only(
+            Combinators.BACKWARD_COMPOSITION.value,
+            [(CAT["Y/Z"], CAT["X\\Y"], CAT["X/Z"]), (CAT["(X/Y)\\(X/Y)"], CAT["(X/Y)\\(X/Y)"], CAT["(X/Y)\\(X/Y)"])],
+        )
+
+    def test_forward_type_raising(self):
+        combines_only(
+            Combinators.FORWARD_TYPE_RAISE.value, [(CAT["X"], CAT["(Y\\X)/Z"], unwrap_builder(Y << (X >> Y)))]
+        )
+
+    def test_backward_type_raising(self):
+        combines_only(
+            Combinators.BACKWARD_TYPE_RAISE.value, [(CAT["(X/Y)\\(X/Y)"], CAT["Y"], unwrap_builder((X << Y) >> X))]
+        )
 
 
 def get_expected_result(left, right, expected_combinations):
@@ -44,7 +86,7 @@ def get_expected_result(left, right, expected_combinations):
 
 def combines_only(rule, expected_combinations):
     # all left right combinations
-    all_combinations = list(itertools.product(CATEGORIES.values(), CATEGORIES.values()))
+    all_combinations = list(itertools.product(CAT.values(), CAT.values()))
     # in both permutations
     all_combinations = itertools.chain(all_combinations, [(r, l) for l, r in all_combinations])
 
@@ -53,43 +95,10 @@ def combines_only(rule, expected_combinations):
             if not rule.can_combine(left, right):
                 print(left, right)
             assert rule.can_combine(left, right), f"cannot combine {left}, {right}"
+
+            actual_result = next(rule.combine(left, right))
             assert (
-                next(rule.combine(left, right)) == expected_result
-            ), f"incorrect result for combining {left} and {right} != {expected_result}"
+                actual_result == expected_result
+            ), f"incorrect result for combining {left} and {right}: {actual_result} != {expected_result}"
         else:
             assert not rule.can_combine(left, right), f"did not expect to be able to combine {left} {right}"
-
-
-class TestCombinators:
-    def test_forward_application(self):
-        combines_only(
-            Combinators.FORWARD_APPLICATION.value,
-            [
-                (CATEGORIES["X/Y"], Y, X),
-                (CATEGORIES["Y/Z"], Z, Y),
-                (CATEGORIES["X/(Y/Z)"], CATEGORIES["Y/Z"], X),
-                (CATEGORIES["X/Z"], Z, X),
-            ],
-        )
-
-    def test_backward_application(self):
-        combines_only(
-            Combinators.BACKWARD_APPLICATION.value,
-            [(Y, CATEGORIES["X\\Y"], X), (CATEGORIES["Y/Z"], CATEGORIES["X\\(Y/Z)"], X)],
-        )
-
-    def test_forward_composition(self):
-        combines_only(
-            Combinators.FORWARD_COMPOSITION.value, [(CATEGORIES["X/Y"], CATEGORIES["Y/Z"], CATEGORIES["X/Z"])]
-        )
-
-    def test_backward_composition(self):
-        combines_only(
-            Combinators.BACKWARD_COMPOSITION.value, [(CATEGORIES["Y/Z"], CATEGORIES["X\\Y"], CATEGORIES["X/Z"])]
-        )
-
-    def test_forward_type_raising(self):
-        combines_only(Combinators.FORWARD_TYPE_RAISE.value, [])
-
-    def test_forward_type_raising(self):
-        combines_only(Combinators.BACKWARD_TYPE_RAISE.value, [])
